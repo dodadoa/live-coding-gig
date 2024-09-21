9e7267d1b092242025a67b56d44a26cde04a96e9
Wasawat Somno <wasawat.som@cjexpress.co.th>
2024-09-21 15:26:10 +0700

    update

--- hydra-gig/openalpha/perform.js
+++ hydra-gig/openalpha/perform.js
@@ -1,5 +1,6 @@
 bpm = 160
-speed = 0.1
+speed = 1
+
 
 osc(10, 0.1, 10)
   .mask(
@@ -8,14 +9,14 @@ osc(10, 0.1, 10)
       .rotate(() => time / 2)
       .modulateScale(noise(0.3))
   )
-  .colorama(1)
+  // .colorama(1)
   .modulateRotate(osc([100, 20, 1].fast()))
   .color(1, [0, 0.2,1], [0, 1])
   .scroll([2, 4, 6], [2, 3, 4])
   .modulate(o0)
   .diff(o1)
   .brightness(-0.15)
-  .thresh(0.2)
+  // .thresh(0.2)
   .out(o0)
 
   osc(10, 1, 10)
