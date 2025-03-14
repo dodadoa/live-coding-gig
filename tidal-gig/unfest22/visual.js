4a9fd31a2fb4d5000c7bdb625a98613b0490ac5a
wasin somno <wasawat.somno@gmail.com>
2025-03-14 19:59:01 +0700

    update

--- tidal-gig/unfest22/visual.js
+++ tidal-gig/unfest22/visual.js
@@ -5,8 +5,8 @@ noise(2)
 .color(1,0,0)
 .luma(0.01)
 .modulate(o0, 0.2)
-.luma(0.1)
-.layer(solid(0,1,0).diff(gradient(10).color(1,0,1)).luma([1,1,1,1,1,1,1,0].fast()))
+.luma(0.35)
+// .layer(solid(0,1,0).diff(gradient(10).color(1,0,1)).luma([1,1,1,1,1,1,1,0].fast()))
 .out()
 bpm = 130
 speed = 2
